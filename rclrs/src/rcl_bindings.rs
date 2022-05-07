ccbfa961a76cdace3b70b3e860a7a2ca351671a4
SoyaOhnishi <soya.ohnishi@gmail.com>
2022-05-07 03:15:41 +0000

    Add Clone impl for rcutils_allocator_t
    This is required because `set_arguments` method in NodeOption
    requires value allocator (not reference) and
    bindgen does not derive Clone automatically.

--- rclrs/src/rcl_bindings.rs
+++ rclrs/src/rcl_bindings.rs
@@ -5,3 +5,21 @@
 #![allow(clippy::all)]
 
 include!(concat!(env!("OUT_DIR"), "/rcl_bindings_generated.rs"));
+
+impl Clone for rcutils_allocator_t {
+    fn clone(&self) -> Self {
+        let allocate = self.allocate.clone();
+        let deallocate = self.deallocate.clone();
+        let reallocate = self.reallocate.clone();
+        let zero_allocate = self.zero_allocate.clone();
+        let state = self.state.clone();
+
+        rcutils_allocator_t {
+            allocate,
+            deallocate,
+            reallocate,
+            zero_allocate,
+            state,
+        }
+    }
+}
